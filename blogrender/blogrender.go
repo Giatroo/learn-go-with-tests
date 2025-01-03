26833d8d9edb02947d572735cf6c138173a85739
Lucas Paiolla <lucaspaiolla@gmail.com>
2025-01-02 22:26:09 -0300

    Chapter "Templating"

---
+++ blogrender/blogrender.go
@@ -0,0 +1,43 @@
+package blogrender
+
+import (
+	"embed"
+	"html/template"
+	"io"
+	"strings"
+)
+
+type Post struct {
+	Title, Description, Body string
+	Tags                     []string
+}
+
+type PostRenderer struct {
+	templ *template.Template
+}
+
+var (
+	//go:embed "templates/*"
+	postTemplate embed.FS
+)
+
+func NewPostRenderer() (*PostRenderer, error) {
+	templ, err := template.ParseFS(postTemplate, "templates/*.gohtml")
+	if err != nil {
+		return nil, err
+	}
+
+	return &PostRenderer{templ: templ}, nil
+}
+
+func (p Post) SanitiseTitle() string {
+	return strings.ToLower(strings.Replace(p.Title, " ", "-", -1))
+}
+
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+	return r.templ.ExecuteTemplate(w, "blog.gohtml", p)
+}
+
+func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
+	return r.templ.ExecuteTemplate(w, "index.gohtml", posts)
+}
